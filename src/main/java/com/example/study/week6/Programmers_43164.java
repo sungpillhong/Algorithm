05aff7bf3b8c96ceee49179591ae49ec415628bf
hongsungpill <yous3163@gmail.com>
2021-03-01 17:40:07 +0900

    프로그래머스 여행경로 문제 수정

--- src/main/java/com/example/study/week6/Programmers_43164.java
+++ src/main/java/com/example/study/week6/Programmers_43164.java
@@ -7,41 +7,32 @@ import java.util.List;
 //https://programmers.co.kr/learn/courses/30/lessons/43164
 public class Programmers_43164 {
 
-    static boolean visited[]; //티켓을 사용했는지 안했는지 여부
+    static boolean ticketUse[]; //티켓을 사용했는지 안했는지 여부
     static List<String> list = new ArrayList<>();
 
     public String[] solution(String[][] tickets) {
 
-        visited = new boolean[tickets.length];
+        ticketUse = new boolean[tickets.length];
 
-        for(int i = 0 ; i < tickets.length; i++) {
-            String from = tickets[i][0]; //출발
-            String to = tickets[i][1]; //도착
-            if(from.equals("ICN")) { //인천이 시작일때
-                visited[i] = true; //첫 티켓사용
+        dfs(tickets,"ICN","ICN",0); //ICN에서 출발하니깐
 
-                dfs(tickets,from + ",", to, 1);
-                visited[i] = false;
-            }
-        }
         Collections.sort(list); //정렬
         String[] answer = list.get(0).split(",");
         return answer;
     }
-    // ["ICN", "COO", "ICN", "BOO", "DOO"]
-    public static void dfs(String[][] tickets, String route, String end, int cnt ) {
+
+    public static void dfs(String[][] tickets, String startAirPort, String destinationAirPort, int cnt ) {
         if (cnt == tickets.length) { //다돌았으면 문자열을 list에 넣는다.
-            list.add(route + end);
+            list.add(destinationAirPort);
             return;
         }
         for (int i = 0; i < tickets.length; i++) {
-            String from = tickets[i][0]; //출발
-            String to = tickets[i][1]; //도착
-            if (end.equals(from) && !visited[i]) { //도착한 공항과 시작되는공항이 같고 방문하지않았으면
-                visited[i] = true;
-                cnt++;
-                dfs(tickets, route + end + ",", to, cnt); //갱신하면서 재귀
-                visited[i] = false;
+            String startAirPort2 = tickets[i][0]; //출발공항
+            String nextAirPort = tickets[i][1]; //출발공항의도착지
+            if (startAirPort2.equals(startAirPort) && !ticketUse[i]) { //도착한 공항과 시작되는공항이 같고 티켓을 사용안했다면
+                ticketUse[i] = true;
+                dfs(tickets, nextAirPort, destinationAirPort + "," + nextAirPort, cnt+1);
+                ticketUse[i] = false;
             }
         }
     }
