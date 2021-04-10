e59ac6e57b2cb198be244e8078a7aa9f9520e378
hongsungpill <yous3163@gmail.com>
2021-04-10 22:55:44 +0900

    리트코드 - 그레이코드 설명추가

--- src/main/java/com/example/study/week11/LeetCode_graycode.java
+++ src/main/java/com/example/study/week11/LeetCode_graycode.java
@@ -15,11 +15,24 @@ public class LeetCode_graycode {
             gray.add(1);
             return gray;
         }
+        /**
+         * n이 3일경우
+         * 000 000
+         * 001 001
+         *
+         * 011 010+001
+         * 010 010+000
+         *
+         * 110 100+010
+         * 111 100+011
+         * 101 100+001
+         * 100 100+000
+         */
         else{ //n이 2이상일때
-            int grayCode = 1 << n-1; //시프트연산자로 n-1만큼 왼쪽으로 이동 즉 2의제곱승
-            gray = grayCode(n-1);
+            int firstGrayCode = 1 << n-1; //시프트연산자로 n-1만큼 왼쪽으로 이동. 즉, 2의제곱승
+            gray = grayCode(n-1); //
             for(int i = gray.size()-1; i >=0; i--){
-                gray.add(grayCode+gray.get(i));
+                gray.add(firstGrayCode+gray.get(i)); //firstGrayCode값 뒤로 이전의 grayList에있는값들을 더해서 넣어준다.
             }
             return gray;
         }
