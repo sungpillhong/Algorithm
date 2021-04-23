620d419da579d874599a594366fcf9178402a861
hongsungpill <yous3163@gmail.com>
2021-04-23 21:15:11 +0900

    해커랭커 문제 업로드

---
+++ src/main/java/com/example/study/week13/Hacker_countluck.java
@@ -0,0 +1,13 @@
+package com.example.study.week13;
+
+import java.util.List;
+
+//https://www.hackerrank.com/challenges/count-luck/problem
+public class Hacker_countluck {
+
+    public String countLuck(List<String> matrix, int k){
+
+        String answer = "";
+        return answer;
+    }
+}
---
+++ src/main/java/com/example/study/week13/Hacker_prisoner.java
@@ -0,0 +1,9 @@
+package com.example.study.week13;
+
+public class Hacker_prisoner {
+    public int saveThePrisoner(int n, int m, int s){
+        int answer = 0;
+
+        return answer;
+    }
+}
